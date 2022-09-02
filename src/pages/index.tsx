672ddeee447f440ae39ae310dda0980556b0660f
unknown <mykael.barnes@hotmail.com>
2022-09-02 02:22:48 -0700

    more changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/dom/Header";
 import dynamic from "next/dynamic";
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Stars, Sky, Text } from '@react-three/drei'
+import { OrbitControls, Stars, Sky, Text, Center, Text3D } from '@react-three/drei'
 import Controller from '../components/Controller'
 import { useState, useEffect } from 'react'
 import { BiHomeHeart } from 'react-icons/bi'
@@ -55,11 +55,51 @@ const Footer = ({active, toggleModal, setToggleModal}) => {
 
 const Modal = (props) => {
 
+  const [selectedReview, setSelectedReview] = useState({
+    name: '',
+    review: '',
+    img: ''
+  })
+
+  useEffect(() => {
+    if (props.toggleModal == 1) {
+      setSelectedReview({
+        name: 'Nathan',
+        review: 'dude wtf this controller you gave me has free dash back out of crouch',
+        img: ''
+      })
+    } else if (props.toggleModal == 2) {
+      setSelectedReview({
+        name: 'Nathan',
+        review: 'dude wtf this controller you gave me has free dash back out of crouch',
+        img: ''
+      })
+    } else if (props.toggleModal == 3) {
+      setSelectedReview({
+        name: 'Nathan',
+        review: 'dude wtf this controller you gave me has free dash back out of crouch',
+        img: ''
+      })
+    } else if (props.toggleModal == 4) {
+      setSelectedReview({
+        name: 'Nathan',
+        review: 'dude wtf this controller you gave me has free dash back out of crouch',
+        img: ''
+      })
+    }
+
+  },[])
+
   return (
 
     <div className="modal-background">
       <div className="modal">
-        
+        <div className="modal-info">
+          <img />
+          <h2>{selectedReview.name}</h2>
+          <p>{selectedReview.review}</p>
+          <span>⭐⭐⭐⭐⭐</span>
+        </div>
 
         <button id='modal-button' onClick={() => props.setToggleModal(0)}>Close</button>
          
@@ -78,6 +118,9 @@ const R3F = ({active, setActive}) => {
         <ambientLight intensity={0.5} />
         <directionalLight position={[-2, 5, 2]} intensity={1} />
         <Controller active={active} setActive={setActive} />
+        <Center top left>
+          <Text>hello</Text>
+        </Center>
     </>
   );
 };
